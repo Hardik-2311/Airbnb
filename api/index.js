86afa50136fce1f57567339c43a13c89eb0cd269
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-07-23 17:35:34 +0530

    database connected with hashed password

--- api/index.js
+++ api/index.js
@@ -1,20 +1,32 @@
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
+require('dotenv').config();
+const User = require('./models/user.js');
+const { default: mongoose } = require('mongoose');
 const app = express();
+var bcrypt = require('bcryptjs');
 
+const bcryptsalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 app.use(cors({
-    credentials: true,
-    origin: 'http://127.0.0.1:5174',
-  }));
+  credentials: true,
+  origin: 'http://127.0.0.1:5174',
+}));
 
-app.get("/test",(req,res)=>{
-   res.json("test ok")
+mongoose.connect(process.env.mongo_url);
+
+app.get("/test", (req, res) => {
+  res.json("test ok")
 })
-app.post("/register",(req,res)=>{
-    const{name,email,password}=req.body;
-    res.json({name,email,password});
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const userData = await User.create({
+    name,
+    email,
+    password:bcrypt.hashSync(password, bcryptsalt),
+  })
+  res.json(userData);
 })
 
 app.listen(4000);
